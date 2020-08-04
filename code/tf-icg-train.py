f946dd5691b100fcbeadc37c2e63c937f57f108f
khademi <mkhademi@sfu.ca>
2020-08-04 02:29:52 -0700

    Update tf-icg-train.py

--- code/tf-icg-train.py
+++ code/tf-icg-train.py
@@ -5,10 +5,10 @@ import numpy
 import pickle
 from models import ICG_model, ICG_model_grid, ICG_model_att
 
-
-tfrecord_dir = "/local-scratch"
-NUM_EPOCHS = 10000
-PRE_TRAINED = True
+tfrecord_dir = "../data"
+NUM_EPOCHS = 100
+VISUAL_CONCEPT_SIZE = 1000
+PRE_TRAINED = False
 rng = numpy.random.RandomState(1234)
 
 f = open('../data/coco_dict.pickle','rb')
@@ -16,17 +16,19 @@ coco_dict = pickle.load(f)
 f.close()
 
 # Setting hyperparameter values
-hyperparameters = {'batch_size': 100, 'save_freq': 1000, 'vocab_size': len(coco_dict), 'model': 'att_icg',
-                   'word_emb_size': 512, 'feat_dim': 2048, 'dropout_keep_rate': 0.5, 'learning_rate': 0.00001
-                   ,'grid_feat_dim': 1024,'grid_size': 14, 'grid_emb_size': 512}
+hyperparameters = {'batch_size': 100, 'save_freq': 100, 'model': 'grid_icg',
+                   'word_emb_size': 512, 'feat_dim': 2048, 'grid_size': 14,
+                   'dropout_keep_rate': 0.5, 'learning_rate': 0.00001,
+                   'grid_feat_dim': 1024, 'grid_emb_size': 512,
+                   'vocab_size': len(coco_dict)}
 feat_dim = hyperparameters['feat_dim']
 grid_feat_dim = hyperparameters['grid_feat_dim']
 grid_size = hyperparameters['grid_size']
 
-
 # Load features from the tf record file
 filename = os.path.join(tfrecord_dir, "coco_train_val_480_514.tfrecords")  
-filename_queue = tf.train.string_input_producer([filename], num_epochs=NUM_EPOCHS)
+filename_queue = tf.train.string_input_producer([filename],
+                                                num_epochs=NUM_EPOCHS)
 reader = tf.TFRecordReader()
 _, serialized_example = reader.read(filename_queue)
 
@@ -37,11 +39,11 @@ features = tf.parse_single_example(
         'target': tf.VarLenFeature(tf.int64),
         'data_type': tf.FixedLenFeature([1], tf.int64),
         'im_id': tf.FixedLenFeature([1], tf.int64),
-        'visual_concept': tf.FixedLenFeature([1000], tf.float32),
-        'grid_feat': tf.FixedLenFeature([grid_feat_dim * grid_size * grid_size], tf.float32),
+        'visual_concept': tf.FixedLenFeature([VISUAL_CONCEPT_SIZE], tf.float32),
+        'grid_feat': tf.FixedLenFeature([grid_feat_dim * grid_size * grid_size],
+                                        tf.float32),
         'feat': tf.FixedLenFeature([feat_dim], tf.float32)      
     })
-
 
 # Loading the model
 if hyperparameters['model'] == 'icg' :    
@@ -63,8 +65,6 @@ if hyperparameters['model'] == 'grid_icg' :
 train_step = model._train_step
 info = model._info
 cross_entropy = model._cross_entropy
-logits= model._logits
-emb = model._W_emb
 
 
 sess = tf.Session()
@@ -73,18 +73,16 @@ sess.run(init)
 init = tf.local_variables_initializer()
 sess.run(init)
 
-
 if PRE_TRAINED:
     saver = tf.train.Saver()
     saver.restore(sess, tf.train.latest_checkpoint(DIR))
 
-
 # Coordinator
 coord = tf.train.Coordinator()
-threads = tf.train.start_queue_runners(sess=sess,coord=coord)
-saver = tf.train.Saver(max_to_keep=1, keep_checkpoint_every_n_hours=1000)
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+saver = tf.train.Saver(max_to_keep=1, keep_checkpoint_every_n_hours=100)
 
-# Run the training session and save the results every save frequency time
+# Run the training session and save the results 
 try:
     step = 0
     total_cost = 0.0
@@ -105,11 +103,3 @@ finally:
 # Wait for threads to finish
 coord.join(threads)
 sess.close()
-
-
-
-
-
-
-
-
